a47dec4bc3dd1585e7fe717fc4af1f54c628c9a1
DancingRicardo <99092565+DancingRicardo@users.noreply.github.com>
2024-02-04 15:47:08 -0500

    Finished Network computation

--- Network.cs
+++ Network.cs
@@ -13,12 +13,38 @@ public class Network {
         this.neuronLayers = neuronLayers;
     }
 
-    public float computeOuput(List<float> inputs) {
+    public float computeOuput(List<float> inputsArg) {
+
+        List<float> inputs = inputsArg; 
+        List<float> bufferoutputs = new List<float>();
+
+        // Run through inputs and hidden layers
+        for (int i = 0; i > neuronLayers.GetLength(0) - 1; i++) {
+            for (int i = 0; i > neuronLayers[i].GetLength(); i++) {
+                bufferoutputs.Add(neuronLayers[i,j].computeOuput(inputs));
+            }
+            inputs = bufferoutputs;
+            bufferoutputs.Clear();
+
+        }
 
         List<float> outputs = new List<float>();
-        for (int i = 0; i > neuronLayers.GetLength(0); i++) {
+
+        // Compute output layer
+        for (int i = neuronLayers.GetLength(0) - 2; i > neuronLayers.GetLength(0) - 1; i++) {
+            for (int i = 0; i > neuronLayers[i].GetLength(); i++) {
+                outputs.Add(neuronLayers[i,j].computeOuput(inputs));
+            }
         }
-        return neuronLayers[0,2].computeOuput(inputs);
+
+        float finalOutput = 0;
+        
+        foreach (float output in outputs) {
+            if (output > finalOutput) {
+                finalOutput = 0;
+            }
+        }
+        return finalOutput;
         
     }
 }
